76573efae7d9c30a193a87199d79b3cf8da714b7
Firas <Firaskamil@gmail.com>
2026-03-04 22:50:17 +0100

    Initial Commit

---
+++ Youtube downloader/Youtube_downloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_downloaderApp.swift
+//  Youtube downloader
+//
+//  Created by User on 2026-03-04.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_downloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
